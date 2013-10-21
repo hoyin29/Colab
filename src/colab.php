547c22aa0e03f880a7f110268ef7c03efe942895
= <hoyin29@gmail.com>
2013-10-21 09:55:27 -0400

    updated db and queries for post

--- src/colab.php
+++ src/colab.php
@@ -1,28 +1,91 @@
 <?php
 	include 'db_helper.php';
 	
-	function getLoginUsername()
+	function listComments($postId)
 	{
-		global $_USER;
-		echo $_USER['uid'];
+		$dbQuery = sprintf("SELECT * FROM comment WHERE postId=" . $postId . " ORDER BY time ASC");
 
-		/*
-		global $_PLATFORM;
-		$result = $_PLATFORM->secureWeb("https://shepherd.cip.gatech.edu/proxy/?url=".urlencode("https://pinch1.lms.gatech.edu/sakai-login-tool/container"));
-		$result = $_PLATFORM->secureWeb("https://shepherd.cip.gatech.edu/proxy/?url=".urlencode("https://t-square.gatech.edu/direct/site.json"));
-		*/
-			
+		$result = getDBResultsArray($dbQuery);
+
+		header("Content-type: application/json");
+		echo json_encode($result);
+	}
+	
+	function getComment($postId, $commentId)
+	{
+		$dbQuery = sprintf("SELECT * FROM comment WHERE postId=" . $postId . " AND commentId=" . $commentId);
+
+		$result = getDBResultsArray($dbQuery);
+		
+		header("Content-type: application/json");
+		echo json_encode($result);
+	}
+	
+	function addComment($postId)
+	{
+	
+	}
+	
+	function deleteComment($postId)
+	{
+	
+	}
+	
+	function listPosts($courseId)
+	{
+		$dbQuery = sprintf("SELECT * FROM post WHERE courseId=" . $courseId . " ORDER BY time ASC");
+
+		$result = getDBResultsArray($dbQuery);
+		
+		header("Content-type: application/json");
+		echo json_encode($result);
+	}
+	
+	function getPost($courseId, $postId)
+	{
+		$dbQuery = sprintf("SELECT * FROM post WHERE courseId=" . $courseId . " AND postId=" . $postId);
+
+		$result = getDBResultsArray($dbQuery);
+		
+		header("Content-type: application/json");
+		echo json_encode($result);
+	}
+	
+	function addPost($courseId)
+	{
+
+	}
+	
+	function deletePost($courseId)
+	{
+	
+	}
+	
+	function getCoursesFromTS()
+	{
 		/*
 		global $_PLATFORM;
 		$result = $_PLATFORM->secureWeb("https://shepherd.cip.gatech.edu/proxy/?url=".urlencode("https://pinch1.lms.gatech.edu/sakai-login-tool/container"));
 		$result = $_PLATFORM->secureWeb("https://shepherd.cip.gatech.edu/proxy/?url=".urlencode("https://pinch1.lms.gatech.edu/direct/user/current.json"));
-		//var_dump($result);
-		
-		if($result == null)
-			echo json_encode($_USER['uid']);
-		else
-			echo $result;
+		var_dump($result);
 		*/
+		
+		global $_PLATFORM;
+		//$result = $_PLATFORM->secureWeb("https://shepherd.cip.gatech.edu/proxy/?url=".urlencode("https://pinch1.lms.gatech.edu/sakai-login-tool/container"));
+		//$result = $_PLATFORM->secureWeb("https://shepherd.cip.gatech.edu/proxy/?url=".urlencode("https://t-square.gatech.edu/direct/site.json"));
+
+		header("Content-type: application/json");
+		echo $result;
+	}
+	
+	function getLoginUsername()
+	{
+		global $_USER;
+		$dbQuery = sprintf("SELECT studentId FROM student WHERE studentGt='" . $_USER['uid'] . "' LIMIT 1");
+		$result = getDBResultsArray($dbQuery);
+		$selfId = $result[0]['studentId'];
+		header("Content-type: application/txt");
+		echo $selfId;
 	}
 	
 	/*
@@ -31,7 +94,7 @@
 	function listCourses($studentId)
 	{
 		$dbQuery = sprintf("SELECT * FROM course WHERE courseId IN
-			(SELECT courseId FROM studentCourseMapping WHERE studentId=" . $studentId . ")");
+			(SELECT courseId FROM studentCourseMapping WHERE studentId=" . $studentId . ") ORDER BY courseName ASC");
 			
 		$result = getDBResultsArray($dbQuery);
 		
@@ -113,8 +176,8 @@
 	
 	function listFriends($courseId)
 	{
-		$dbQuery = sprintf("SELECT studentFirst, studentLast FROM student WHERE studentId IN
-			(SELECT studentId FROM studentCourseMapping WHERE courseId=" . $courseId . ")");
+		$dbQuery = sprintf("SELECT studentFirst, studentLast, studentId FROM student WHERE studentId IN
+			(SELECT studentId FROM studentCourseMapping WHERE courseId=" . $courseId . ") ORDER BY studentFirst ASC");
 
 		$result = getDBResultsArray($dbQuery);
 		
